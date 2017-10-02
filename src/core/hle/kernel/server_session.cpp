b7b910a71483f97ca19039443d4d8100e4f56a6b
Subv <subv2112@gmail.com>
2017-10-01 22:09:00 -0500

    Kernel/IPC: Put a thread to sleep when performing an LLE IPC request.
    It will be awoken when the server replies to the request via svcReplyAndReceive.

--- src/core/hle/kernel/server_session.cpp
+++ src/core/hle/kernel/server_session.cpp
@@ -66,6 +66,8 @@ ResultCode ServerSession::HandleSyncRequest(SharedPtr<Thread> thread) {
         // Add the thread to the list of threads that have issued a sync request with this
         // server.
         pending_requesting_threads.push_back(std::move(thread));
+        // Put the thread to sleep until the server replies, it will be awoken in svcReplyAndReceive.
+        thread->status = THREADSTATUS_WAIT_IPC;
     }
 
     // If this ServerSession does not have an HLE implementation, just wake up the threads waiting
